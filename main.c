2b45b1fabf0184a52a8a1b9dc8b24be9463f56c1
nikhome <tutu@lutu.rr>
2025-12-24 12:35:58 +0200

    добавленг решение в main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+{//#1
+int a[n1], i ;
+double  sr = 0, k = 0;
+setlocale(LC_ALL, "rus");
+srand(time(NULL));
+for (i = 0; i < n1; i++)
+{
+	a[i] = rand()%10;
+	printf("%i\t", a[i]);
+}
+for (i = 0; i < n2; i++)
+	sr += a[i];
+sr = sr / n2;
+printf("\nСреднее арифметическое = %.2lf", sr);
+_getch();
+}
